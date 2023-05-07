6b3b79f6be16e111bef08fe51bfe9010b63daebb
saurrravvv <sauravgusain392@gmail.com>
2023-05-07 19:41:30 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Practice</title>
+	<link rel="stylesheet" type="text/css" href="custom.css">
+</head>
+<body>
+	<div class="new cyz">
+		<h2>Gaurav</h2>
+	</div>
+	<div style="margin-top: 10px;">
+		<h1>Gaurav</h1>
+		<h2>Gaurav</h2>
+		<h3>Gaurav</h3>
+		<h4>Gaurav</h4>
+		<h5>Gaurav</h5>
+		<h6>Gaurav</h6>
+	</div>
+	<div style="border:  2px solid black;">
+		<span style="border: 4px solid black;">Gaurav</span>
+		<span style="border: 4px solid black;">Gaurav</span>
+		<span style="border: 4px solid black;">Gaurav</span>
+		<span style="border: 4px solid black;">Gaurav</span>
+	</div>
+	<div>
+		<p>ahskjdfh;kasndfklas;kfdkasdfk;haskjdfh;kasjhdfhasdxfxn.kasjd.</p>
+	</div>
+
+	<?php
+
+		$students = [
+			"1"=>"Gaurav",
+			"2"=>"Saurav",
+			"3"=>"Rahul",
+			"4"=>"Vishwas",
+			"5"=>"Random",
+			"6"=>"Gaurav",
+		]
+
+	?>
+
+	<table cellpadding="4px" border="1" cellspacing="0">
+		<thead>
+			<th>Sn.</th>
+			<th>Name</th>
+			<th>Class</th>
+			<th>Roll no</th>
+		</thead>
+		<tbody>
+			<td>1</td>
+			<td>Gaurav</td>
+			<td>BCA</td>
+			<td>12</td>
+		</tbody>
+	</table>
+
+	
+</body>
+</html>
